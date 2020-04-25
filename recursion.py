8aba35dde21b7ee98a69132e95ea403b7fddf902
progbash <amorfrap@gmail.com>
2020-04-26 02:46:01 +0400

    Added power function using multiply function

--- recursion.py
+++ recursion.py
@@ -3,29 +3,45 @@
 
 # %% reverse string
 
+
 def reverse(string):
     if len(string) <= 1:
         return string
     return reverse(string[1:]) + string[0]
+
 
 print(reverse("aqaq anad ridaltrap narmak"))
 
 
 # %% multiplication with RECURSION !
 
-def multiply(x ,y):
+def multiply(x, y):
     if y == 0:
         return 0
     else:
         return x + multiply(x, y-1)
 
-print(multiply(4,7))
 
+print(multiply(4, 7))
 
 # %% quvvete yukseltmation with RECURSION !!!
+
 
 def power(x, y):
     if y == 0:
         return 1
-    return x*power(x,y-1)
-print(power(5,2))
+    return x*power(x, y-1)
+
+
+print(power(5, 2))
+
+# %% power function using multiply function))
+
+
+def power_another(x, y):
+    if y == 0:
+        return 1
+    return multiply(x, power_another(x, y-1))
+
+
+print(power_another(5, 3))
